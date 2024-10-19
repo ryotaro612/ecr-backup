b407f575a6054107b172b0d98b525fd0c827e899
Ryotaro <ryotaro612.ooo@gmail.com>
2024-10-20 01:02:37 +0900

    feat: add makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,22 @@
-.PHONY: help test
+PROJECT_ROOT := $(dir $(lastword $(MAKEFILE_LIST)))
+.PHONY: help test all
 
-help: ## Display this message
+help: ## display this message
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+DIST_DIR := $(PROJECT_ROOT)dist
+BIN := $(DIST_DIR)/ecrbk
+all: $(BIN) ## build the artifacts
 
 test: ## Run the tests
 	go test -tags test -v ./...
+
+
+clean: ## delete the intermediate files
+	rm -rf $(DIST_DIR)
+
+$(BIN): $(shell find . -type f -regex "[^#]+\.go")
+	mkdir -p $(DIST_DIR)
+	go build -o $(BIN) ./cmd/ecrbk.go
+
+
--- cmd/ecrbk.go
+++ cmd/ecrbk.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
-	"github.com/docker/docker/client"
+	_ "github.com/docker/docker/client"
 	"log/slog"
 	"os"
 )
@@ -50,6 +50,13 @@ func main() {
 	if err != nil {
 		logger.ErrorContext(ctx, "listing the images was failed", "error", err, "repository", repositoryName)
 	}
+
+	token, err := cli.GetAuthorizationToken(ctx, &ecr.GetAuthorizationTokenInput{})
+	if err != nil {
+		logger.ErrorContext(ctx, "getting the authorization token was failed", "error", err)
+		os.Exit(1)
+	}
+	fmt.Println(token)
 
 	for _, image := range images.ImageIds {
 		if tag := image.ImageTag; tag != nil {
